4f9f70cb370325c01ec8031df827c4595fabd2b1
Markandu Satbirajah <bmarks@bbc-be.local>
2018-04-19 09:44:02 +0200

    Das Dropdown bekomme ich nicht hin

--- view/buch_create.php
+++ view/buch_create.php
@@ -11,13 +11,17 @@
  * Time: 09:22
  */
 
-
+// https://www.wirlernen.at/PHP5fuerEinsteiger/049.php
 
 $form = new Form('/buch/createBook');
 
+echo "<select> <option value='$genre'></option></select>";
+echo $form->file()->label('bild')->name('bild');
 echo $form->text()->label('titel')->name('titel');
 echo $form->text()->label('autor')->name('autor');
-echo $form->password()->label('Passwort')->name('passwort');
+echo $form->text()->label('datum')->name('datum');
+echo $form->text()->label('zusammenfassung')->name('zusammenfassung');
+
 echo $form->submit()->label('Buch hinzufüen')->name('send');
 
 $form->end();
